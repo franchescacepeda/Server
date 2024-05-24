19ee158b4764b7851854ebc16798d4fb8876837d
Franchesca Cepeda <31700834+franchescacepeda@users.noreply.github.com>
2024-05-24 15:53:28 -0400

    Created a static asset to retun an image

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const express = require('express')
 const app = express()
 
 const port = 300
+app.use(express.static(path.join(public)))
 
 app.get('/test', (req, res) => {
     res.send('Server is operational');
 })
+
+app.listen(port, () =>{
+    console.log(`Server is running on port ${port}`)
+})
+
