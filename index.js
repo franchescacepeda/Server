3a7eae1d5f353c04920152c50e8d2eed61b95901
Franchesca Cepeda <31700834+franchescacepeda@users.noreply.github.com>
2024-05-24 16:26:50 -0400

    Added the dist folder as a static path and returned

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ app.get('/test', (req, res) => {
     res.send('Server is operational');
 })
 
+const root = (express.static(path.join(__dirname, '../client/dist')))
+app.use(express.static(root))
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html', {root})
+})
 
 app.listen(port, () =>{
     console.log(`Server is running on port ${port}`)
